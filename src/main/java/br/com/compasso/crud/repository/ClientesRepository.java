e0d3b0e1e671293a0aab32c0f6a62964f54faccf
Vinicius Zanoli <viniciusdasilva.92hotmail.com>
2021-01-18 22:39:44 -0300

    Suporte ao docker

--- src/main/java/br/com/compasso/crud/repository/ClientesRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.compasso.crud.repository;
-
-import br.com.compasso.crud.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientesRepository extends JpaRepository<Cliente, Long> {
-  Cliente findByNomeCompleto(String nome);
-}
